f9c0d0a859f63d45932b6f3e67eae2eb325517bd
felipeB2not <felipereis4k@gmail.com>
2025-04-04 20:51:32 -0300

    grafico popularidade

--- Angular dashboard animes/dashboard-animes/src/app/components/charts/popularidade-tempo/popularidade-tempo.component.ts
+++ Angular dashboard animes/dashboard-animes/src/app/components/charts/popularidade-tempo/popularidade-tempo.component.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexDataLabels,
+  ApexYAxis, ChartComponent
+} from 'ng-apexcharts';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import {CommonModule, NgIf} from '@angular/common';
+import {AnimeService} from '../../../services/anime.service';
 
 @Component({
   selector: 'app-popularidade-tempo',
-  imports: [],
+  imports: [
+    ChartComponent,
+    NgApexchartsModule,
+    NgIf
+  ],
+  standalone: true,
   templateUrl: './popularidade-tempo.component.html',
   styleUrl: './popularidade-tempo.component.scss'
 })
-export class PopularidadeTempoComponent {
+export class PopularidadeTempoComponent implements OnInit{
+  series: ApexAxisChartSeries = [];
+  chart: ApexChart = {
+    type: 'line',
+    height: 350
+  };
+  title: ApexTitleSubtitle = {
+    text: 'Popularidade Média por Temporada'
+  };
+  xaxis: ApexXAxis = {
+    categories: [],
+    title: {
+      text: 'Temporada'
+    }
+  };
+  yaxis: ApexYAxis = {
+    title: {
+      text: 'Popularidade'
+    }
+  };
+  stroke: ApexStroke = {
+    curve: 'smooth'
+  };
+  dataLabels: ApexDataLabels = {
+    enabled: true
+  };
 
+  constructor(private animeService: AnimeService) {}
+
+  ngOnInit(): void {
+    this.animeService.getPopularidadeTempo().subscribe((data) => {
+      const temporadas = data.popularidade_media_temporada.map((item: any) => item.temporada);
+      const medias = data.popularidade_media_temporada.map((item: any) => item.popularidade);
+
+      this.series = [
+        {
+          name: 'Popularidade média',
+          data: medias
+        }
+      ];
+      this.xaxis.categories = temporadas;
+    });
+  }
 }
--- Angular dashboard animes/dashboard-animes/src/app/components/sidebar/sidebar.component.ts
+++ Angular dashboard animes/dashboard-animes/src/app/components/sidebar/sidebar.component.ts
@@ -23,12 +23,9 @@ import { RouterModule } from '@angular/router';
 export class SidebarComponent {
   items = [
     { label: 'Mais assistidos', icon: 'visibility', value: 'mais-assistidos' },
-    { label: 'Top assistidos', icon: 'leaderboard', value: 'top-assistidos' },
     { label: 'Distribuição por gênero', icon: 'pie_chart', value: 'distribuicao-genero' },
     { label: 'Popularidade no tempo', icon: 'timeline', value: 'popularidade-tempo' },
-    { label: 'Correlação entre métricas', icon: 'insights', value: 'correlacoes' },
     { label: 'Score vs Popularidade', icon: 'scatter_plot', value: 'score-vs-popularidade' },
     { label: 'Comparar Tipos', icon: 'hub', value: 'comparar-tipos' },
-    { label: 'Tabela Interativa', icon: 'table_chart', value: 'tabela' }
   ];
 }
